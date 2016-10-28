4f4f18b34b92bc53adfbf913b24b31fc46be38dc
Nowacki Frederic <fder10@hotmail.fr>
2016-10-28 20:02:35 +0200

    MessageEvent and Configuration to add token in appconfig

---
+++ capibot.core/Tools/Parser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capibot.Core.Tools
+{
+    public static class Parser
+    {
+
+        public static void Text(string text)
+        {
+
+        }
+
+    }
+}
---
+++ capibot.discord/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace Capibot.Discord
+{
+    public static class Config
+    {
+        public static string GetToken()
+        {
+            var section = ConfigurationManager.GetSection("DiscordToken") as NameValueCollection;
+            return section["token"];
+        }
+
+    }
+}
